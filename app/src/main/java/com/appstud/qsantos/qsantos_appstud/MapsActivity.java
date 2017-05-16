8b75d89eee434ec4c6961d6021b93d814b291e53
QsantosTest <Vemaother@gmail.com>
2017-05-16 19:07:44 +0200

    Display actual position / setLocation instead of marker on the map / refresh actual position

--- app/src/main/java/com/appstud/qsantos/qsantos_appstud/MapsActivity.java
+++ app/src/main/java/com/appstud/qsantos/qsantos_appstud/MapsActivity.java
@@ -7,10 +7,8 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
@@ -20,7 +18,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -37,46 +34,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Location location;
+        final Location location;
 
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            //public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //int[] grantResults)
+            /*public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            int[] grantResults) {*/
             Log.i(TAG, "No permission");
             return;
         }
+        //Check if GPS is activated.
+        if (CheckPos(locationManager) != 0) {
+            return;
+        }
 
-        CheckPos(locationManager);
         location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Actual position"));
+        //mMap.addMarker(new MarkerOptions().position(latLng).title("Actual position"));
         mMap.animateCamera(cameraUpdate);
+        mMap.setMyLocationEnabled(true);
+
         final LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                 CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
                 mMap.animateCamera(cameraUpdate);
+                mMap.setMyLocationEnabled(true);
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
+                onLocationChanged(location);
             }
 
             public void onProviderEnabled(String provider) {
